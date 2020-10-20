5ce0889ec21d7b92b792fe3f4b0db1251dee7bac
beyarkay <boydrkane@gmail.com>
2020-10-20 19:18:05 +0200

    register a client script started

--- register_client.php
+++ register_client.php
@@ -7,14 +7,35 @@ function isValidJSON($str) {
    json_decode($str);
    return json_last_error() == JSON_ERROR_NONE;
 }
-
 $json_params = file_get_contents("php://input");
 
+// check that the username is good
 if (strlen($json_params) > 0 && isValidJSON($json_params)){
-  $decoded_params = json_decode($json_params, true);
-  $fp = fopen('new_nodes.txt', 'a');
-  fwrite($fp, $decoded_params['root'].PHP_EOL);
-  fclose($fp);   
-}
+    $decoded_params = json_decode($json_params, true);
+    // Get the proposed username
+    $username = $decoded_params['username'];
+    // Get existing usernames
+    $server_json_str = file_get_contents('server_state.json');
+    $server_json = json_decode($server_json_str, true);
+    $return_state = ['state' => 'good'];
+    foreach ((array) $server_json['clients'] as $client) {
+        if($client['username'] == $username) {
+            header('Content-type:application/json;charset=utf-8');
+            $return_state = [
+                'state' => 'error',
+                'cause' => 'username already taken'
+            ];
+            break;
+        }
+    }
+    if ($return_state["state"] == "good") {
+        $file = "server_state.json";
+        $server_state = json_decode(file_get_contents($file), TRUE);
 
+        array_push($server_state["clients"], $decoded_params);
+
+        file_put_contents($file, json_encode($server_state));
+    }
+    echo json_encode( $return_state );
+}
 ?>
