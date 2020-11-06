c08c98ade088f2939676d1740df3f20ec603fb53
beyarkay <boydrkane@gmail.com>
2020-11-07 00:29:00 +0200

    got get_gamestate transmitting the patch notes

--- get_gamestate.php
+++ get_gamestate.php
@@ -3,15 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// function isValidJSON($str) {
-//    json_decode($str);
-//    return json_last_error() == JSON_ERROR_NONE;
-// }
 $json_params = file_get_contents("php://input");
 $decoded_params = json_decode($json_params, true);
 
-//$from_tick = $decoded_params['from_tick']
-echo json_encode( $decoded_params , JSON_PRETTY_PRINT);
+//echo json_encode( $decoded_params , JSON_PRETTY_PRINT);
 
 // Get the last seen tick from the client
 $last_seen_tick = (int)$decoded_params['last_seen_tick'];
@@ -20,7 +15,7 @@ $last_seen_tick = (int)$decoded_params['last_seen_tick'];
 $game_id = $decoded_params['game_id'];
 
 // Get the current game tick
-echo "getting file games/$game_id/game.json<br>";
+echo "getting file games/$game_id/game.json ....<br>";
 $game_json = file_get_contents("games/$game_id/game.json");
 $game_obj = json_decode($game_json, true);
 $curr_game_tick = (int)$game_obj['tick'];
@@ -29,8 +24,9 @@ echo "Curr game tick is $curr_game_tick<br><br>";
 // Calculate which change files need to be sent
 for ($from = $last_seen_tick; $from < $curr_game_tick; $from ++) {
     $to = $from + 1;
-    echo "From $from to $to: games/$game_id/patch_$from_$to.txt<br>";
-    $contents = file_get_contents("games/$game_id/patch_$from_$to.txt");
+    $path = "games/{$game_id}/patch_{$from}_{$to}.txt"; 
+    echo "From $from to $to: $path<br>";
+    $contents = file_get_contents($path);
     echo $contents;
     echo "<br><br><br>";
 
