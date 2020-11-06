36537047f00b84aaf90daddad61d8581fbc691ab
beyarkay <boydrkane@gmail.com>
2020-11-06 21:16:42 +0200

    .

--- get_gamestate.php
+++ get_gamestate.php
@@ -3,9 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
-
-
 // function isValidJSON($str) {
 //    json_decode($str);
 //    return json_last_error() == JSON_ERROR_NONE;
