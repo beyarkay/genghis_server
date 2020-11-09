abbb4c94b052f1fcf30a940b158d76840df56519
beyarkay <boydrkane@gmail.com>
2020-11-10 00:59:13 +0200

    content

--- get_gamestate.php
+++ get_gamestate.php
@@ -24,8 +24,8 @@ $curr_game_tick = (int)$game_obj['tick'];
 // Calculate which change files need to be sent
 for ($from = $last_seen_tick; $from < $curr_game_tick; $from ++) {
     $to = $from + 1;
-    echo "===$game_id/patch_$from_$to.txt===<br>";
-    $contents = file_get_contents("games/$game_id/patch_$from_$to.txt");
+    echo "===$game_id/patch_${from}_${to}.txt===<br>";
+    $contents = file_get_contents("games/${game_id}/patch_${from}_${to}.txt");
     echo $contents;
     echo "<br>";
 
