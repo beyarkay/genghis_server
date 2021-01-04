c4c2e82133442d02306ca041ef9fe2d8d6c0dcb8
beyarkay <boydrkane@gmail.com>
2021-01-05 00:13:17 +0200

    Feat: Add building on localhost

--- build_test_server.py
+++ build_test_server.py
@@ -1,6 +1,8 @@
 #!/usr/local/bin/python3
 import time
+from pprint import pprint
 import multiprocessing
+from http.server import HTTPServer, CGIHTTPRequestHandler
 import http.server
 import socketserver
 import json
@@ -8,7 +10,14 @@ import os
 import sys
 import util
 
-SERVER_STATE_FILE = 'server_state.json' if len(sys.argv) < 2 else sys.argv[1]
+# Used for SSEHandler:
+import copy
+import select
+import urllib.parse
+from http import HTTPStatus
+
+nobody = None
+SERVER_STATE_FILE = 'server_state.json' if len(sys.argv) == 4 else sys.argv[1]
 
 def main():
     """
@@ -27,9 +36,9 @@ def main():
     ```
     """
     print("At any time, enter a question mark (?) and then press <ENTER> for help.")
-    genghis_client_dir = ""
+    genghis_client_dir = "" if len(sys.argv) != 4 else sys.argv[2]
+    default = '~/genghis_client'
     while not genghis_client_dir:
-        default = '~/genghis_client'
         genghis_client_dir = input("genghis_client directory ['{}']: ".format(default)).strip()
         if genghis_client_dir == "?":
             print("genghis_client Directory Explanation\n" +
@@ -46,9 +55,9 @@ def main():
             # TODO Do some checking for the bot path
     genghis_client_dir = os.path.expanduser(genghis_client_dir)
     
-    repetitions = "" 
+    repetitions = "" if len(sys.argv) != 4 else sys.argv[3]
+    default = "5"
     while not repetitions:
-        default = "3"
         repetitions = input("Number of Repetitions ['{}']: ".format(default)).strip()
         if repetitions == "?":
             print("Repetitions Explanation:\n" + 
@@ -82,7 +91,7 @@ def main():
     # Now build up a server_state.json file
     with open(SERVER_STATE_FILE, 'r') as server_state_file:
         server_state = json.load(server_state_file)
-    server_state['endpoint'] = 'localhost:' + str(GAME_PORT) + "/"
+    server_state['endpoint'] = 'http://localhost:' + str(GAME_PORT) + "/"
     with open(os.path.join(genghis_client_dir, 'config.json'), 'r') as client_config:
         c_config = json.load(client_config)
 
@@ -111,14 +120,359 @@ def start_server(directory='', PORT=8000):
         directory = os.path.dirname(__file__)
 
     os.chdir(directory)
-    Handler = http.server.SimpleHTTPRequestHandler
-    with socketserver.TCPServer(("localhost", PORT), Handler) as httpd:
-        print("Serving at port {}, on directory {}".format(PORT, directory))
+    # Utilities for CGIHTTPRequestHandler
+    def _url_collapse_path(path):
+        """
+        Given a URL path, remove extra '/'s and '.' path elements and collapse
+        any '..' references and returns a collapsed path.
+        Implements something akin to RFC-2396 5.2 step 6 to parse relative paths.
+        The utility of this function is limited to is_cgi method and helps
+        preventing some security attacks.
+        Returns: The reconstituted URL, which will always start with a '/'.
+        Raises: IndexError if too many '..' occur within the path.
+        """
+        # Query component should not be involved.
+        path, _, query = path.partition('?')
+        path = urllib.parse.unquote(path)
+
+        # Similar to os.path.split(os.path.normpath(path)) but specific to URL
+        # path semantics rather than local operating system semantics.
+        path_parts = path.split('/')
+        head_parts = []
+        for part in path_parts[:-1]:
+            if part == '..':
+                head_parts.pop() # IndexError if more '..' than prior parts
+            elif part and part != '.':
+                head_parts.append( part )
+        if path_parts:
+            tail_part = path_parts.pop()
+            if tail_part:
+                if tail_part == '..':
+                    head_parts.pop()
+                    tail_part = ''
+                elif tail_part == '.':
+                    tail_part = ''
+        else:
+            tail_part = ''
+
+        if query:
+            tail_part = '?'.join((tail_part, query))
+
+        splitpath = ('/' + '/'.join(head_parts), tail_part)
+        collapsed_path = "/".join(splitpath)
+
+        return collapsed_path
+
+    nobody = None
+
+    def nobody_uid():
+        """Internal routine to get nobody's uid"""
+        global nobody
+        if nobody:
+            return nobody
         try:
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            pass
-        httpd.server_close()
+            import pwd
+        except ImportError:
+            return -1
+        try:
+            nobody = pwd.getpwnam('nobody')[2]
+        except KeyError:
+            nobody = 1 + max(x[2] for x in pwd.getpwall())
+        return nobody
+
+    def executable(path):
+        """Test for executable file."""
+        return os.access(path, os.X_OK)
+
+    class SSEHandler(http.server.SimpleHTTPRequestHandler):
+
+        """Complete HTTP server with GET, HEAD and POST commands.
+        GET and HEAD also support running CGI scripts.
+        The POST command is *only* implemented for CGI scripts.
+        """
+
+        # Determine platform specifics
+        have_fork = hasattr(os, 'fork')
+
+        # Make rfile unbuffered -- we need to read one line and then pass
+        # the rest to a subprocess, so we can't use buffered input.
+        rbufsize = 0
+
+        def do_POST(self):
+            """Serve a POST request.
+            This is only implemented for CGI scripts.
+            """
+
+            if self.is_cgi():
+                self.run_cgi()
+            else:
+                self.send_error(
+                    HTTPStatus.NOT_IMPLEMENTED,
+                    "Can only POST to CGI scripts")
+
+        def send_head(self):
+            """Version of send_head that support CGI scripts"""
+            if self.is_cgi():
+                return self.run_cgi()
+            else:
+                return http.server.SimpleHTTPRequestHandler.send_head(self)
+
+        def is_cgi(self):
+            """Test whether self.path corresponds to a CGI script.
+            Returns True and updates the cgi_info attribute to the tuple
+            (dir, rest) if self.path requires running a CGI script.
+            Returns False otherwise.
+            If any exception is raised, the caller should assume that
+            self.path was rejected as invalid and act accordingly.
+            The default implementation tests whether the normalized url
+            path begins with one of the strings in self.cgi_directories
+            (and the next character is a '/' or the end of the string).
+            """
+            collapsed_path = _url_collapse_path(self.path)
+            dir_sep = collapsed_path.find('/', 1)
+            while dir_sep > 0 and not collapsed_path[:dir_sep] in self.cgi_directories:
+                dir_sep = collapsed_path.find('/', dir_sep+1)
+            if dir_sep > 0:
+                head, tail = collapsed_path[:dir_sep], collapsed_path[dir_sep+1:]
+                self.cgi_info = head, tail
+                return True
+            return False
+
+
+        cgi_directories = ['/cgi-bin', '/htbin']
+
+        def is_executable(self, path):
+            """Test whether argument path is an executable file."""
+            return executable(path)
+
+        def is_python(self, path):
+            """Test whether argument path is a Python script."""
+            head, tail = os.path.splitext(path)
+            return tail.lower() in (".py", ".pyw")
+
+        def run_cgi(self):
+            """Execute a CGI script."""
+            dir, rest = self.cgi_info
+            path = dir + '/' + rest
+            i = path.find('/', len(dir)+1)
+            while i >= 0:
+                nextdir = path[:i]
+                nextrest = path[i+1:]
+
+                scriptdir = self.translate_path(nextdir)
+                if os.path.isdir(scriptdir):
+                    dir, rest = nextdir, nextrest
+                    i = path.find('/', len(dir)+1)
+                else:
+                    break
+
+            # find an explicit query string, if present.
+            rest, _, query = rest.partition('?')
+
+            # dissect the part after the directory name into a script name &
+            # a possible additional path, to be stored in PATH_INFO.
+            i = rest.find('/')
+            if i >= 0:
+                script, rest = rest[:i], rest[i:]
+            else:
+                script, rest = rest, ''
+
+            scriptname = dir + '/' + script
+            scriptfile = self.translate_path(scriptname)
+            if not os.path.exists(scriptfile):
+                self.send_error(
+                    HTTPStatus.NOT_FOUND,
+                    "No such CGI script (%r)" % scriptname)
+                return
+            if not os.path.isfile(scriptfile):
+                self.send_error(
+                    HTTPStatus.FORBIDDEN,
+                    "CGI script is not a plain file (%r)" % scriptname)
+                return
+            ispy = self.is_python(scriptname)
+            if self.have_fork or not ispy:
+                if not self.is_executable(scriptfile):
+                    self.send_error(
+                        HTTPStatus.FORBIDDEN,
+                        "CGI script is not executable (%r)" % scriptname)
+                    return
+
+            # Reference: http://hoohoo.ncsa.uiuc.edu/cgi/env.html
+            # XXX Much of the following could be prepared ahead of time!
+            env = copy.deepcopy(os.environ)
+            env['SERVER_SOFTWARE'] = self.version_string()
+            env['SERVER_NAME'] = self.server.server_name
+            env['GATEWAY_INTERFACE'] = 'CGI/1.1'
+            env['SERVER_PROTOCOL'] = self.protocol_version
+            env['SERVER_PORT'] = str(self.server.server_port)
+            env['REQUEST_METHOD'] = self.command
+            uqrest = urllib.parse.unquote(rest)
+            env['PATH_INFO'] = uqrest
+            env['PATH_TRANSLATED'] = self.translate_path(uqrest)
+            env['SCRIPT_NAME'] = scriptname
+            if query:
+                env['QUERY_STRING'] = query
+            env['REMOTE_ADDR'] = self.client_address[0]
+            authorization = self.headers.get("authorization")
+            if authorization:
+                authorization = authorization.split()
+                if len(authorization) == 2:
+                    import base64, binascii
+                    env['AUTH_TYPE'] = authorization[0]
+                    if authorization[0].lower() == "basic":
+                        try:
+                            authorization = authorization[1].encode('ascii')
+                            authorization = base64.decodebytes(authorization).\
+                                            decode('ascii')
+                        except (binascii.Error, UnicodeError):
+                            pass
+                        else:
+                            authorization = authorization.split(':')
+                            if len(authorization) == 2:
+                                env['REMOTE_USER'] = authorization[0]
+            # XXX REMOTE_IDENT
+            if self.headers.get('content-type') is None:
+                env['CONTENT_TYPE'] = self.headers.get_content_type()
+            else:
+                env['CONTENT_TYPE'] = self.headers['content-type']
+
+
+            length = self.headers.get('content-length')
+            if length:
+                env['CONTENT_LENGTH'] = length
+            referer = self.headers.get('referer')
+            if referer:
+                env['HTTP_REFERER'] = referer
+            accept = self.headers.get_all('accept', ())
+            env['HTTP_ACCEPT'] = ','.join(accept)
+            ua = self.headers.get('user-agent')
+            if ua:
+                env['HTTP_USER_AGENT'] = ua
+            co = filter(None, self.headers.get_all('cookie', []))
+            cookie_str = ', '.join(co)
+            if cookie_str:
+                env['HTTP_COOKIE'] = cookie_str
+            # XXX Other HTTP_* headers
+            # Since we're setting the env in the parent, provide empty
+            # values to override previously set values
+            for k in ('QUERY_STRING', 'REMOTE_HOST', 'CONTENT_LENGTH',
+                    'HTTP_USER_AGENT', 'HTTP_COOKIE', 'HTTP_REFERER'):
+                env.setdefault(k, "")
+
+#            self.send_header('Content-Type', 'text/event-stream; charset=UTF-8')
+#            self.send_header('Connection', 'Keep-Alive')
+            self.send_response(HTTPStatus.OK, "OK")
+            self.send_header('Cache-Control', 'no-cache')
+            self.send_header('Content-Type', 'text/event-stream;charset=UTF-8')
+            self.send_header('Keep-Alive', 'timeout=5, max=94')
+            self.send_header('Connection', 'Keep-Alive')
+            self.send_header('Transfer-Encoding', 'chunked')
+
+            if hasattr(self, '_headers_buffer'): print(b"".join(self._headers_buffer))
+            self.end_headers()
+
+            decoded_query = query.replace('+', ' ')
+
+            if self.have_fork:
+                # Unix -- fork as we should
+                args = [script]
+                if '=' not in decoded_query:
+                    args.append(decoded_query)
+                nobody = nobody_uid()
+                self.wfile.flush() # Always flush before forking
+                pid = os.fork()
+                if pid != 0:
+                    # Parent
+                    pid, sts = os.waitpid(pid, 0)
+                    # throw away additional data [see bug #427345]
+                    while select.select([self.rfile], [], [], 0)[0]:
+                        if not self.rfile.read(1):
+                            break
+                    exitcode = os.waitstatus_to_exitcode(sts)
+                    if exitcode:
+                        self.log_error(f"CGI script exit code {exitcode}")
+                    return
+                # Child
+                try:
+                    try:
+                        os.setuid(nobody)
+                    except OSError:
+                        pass
+                    os.dup2(self.rfile.fileno(), 0)
+                    os.dup2(self.wfile.fileno(), 1)
+                    os.execve(scriptfile, args, env)
+                except:
+                    self.server.handle_error(self.request, self.client_address)
+                    os._exit(127)
+
+            else:
+                # Non-Unix -- use subprocess
+                import subprocess
+                cmdline = [scriptfile]
+                if self.is_python(scriptfile):
+                    interp = sys.executable
+                    if interp.lower().endswith("w.exe"):
+                        # On Windows, use python.exe, not pythonw.exe
+                        interp = interp[:-5] + interp[-4:]
+                    cmdline = [interp, '-u'] + cmdline
+                if '=' not in query:
+                    cmdline.append(query)
+                self.log_message("command: %s", subprocess.list2cmdline(cmdline))
+                try:
+                    nbytes = int(length)
+                except (TypeError, ValueError):
+                    nbytes = 0
+                p = subprocess.Popen(cmdline,
+                                    stdin=subprocess.PIPE,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                    env = env
+                                    )
+                if self.command.lower() == "post" and nbytes > 0:
+                    data = self.rfile.read(nbytes)
+                else:
+                    data = None
+                # throw away additional data [see bug #427345]
+                while select.select([self.rfile._sock], [], [], 0)[0]:
+                    if not self.rfile._sock.recv(1):
+                        break
+                stdout, stderr = p.communicate(data)
+                self.wfile.write(stdout)
+                if stderr:
+                    self.log_error('%s', stderr)
+                p.stderr.close()
+                p.stdout.close()
+                status = p.returncode
+                if status:
+                    self.log_error("CGI script exit status %#x", status)
+                else:
+                    self.log_message("CGI script exited OK")
+
+
+    attempts = 0
+    while True:
+        assert attempts < 500
+        try:
+            httpd = HTTPServer(("", PORT + attempts), SSEHandler)
+            break
+        except OSError:
+            attempts += 1
+
+    print("serving at port {} on directory {}".format(PORT, directory))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+
+#    Handler = http.server.SimpleHTTPRequestHandler
+#    with socketserver.TCPServer(("localhost", PORT), Handler) as httpd:
+#        print("Serving at port {}, on directory {}".format(PORT, directory))
+#        try:
+#            httpd.serve_forever()
+#        except KeyboardInterrupt:
+#            pass
+#        httpd.server_close()
     os.chdir(old_dir)
 
     
---
+++ cgi-bin/sse.php
@@ -0,0 +1,64 @@
+#!/usr/bin/php
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+date_default_timezone_set("Africa/Johannesburg");
+header("Content-Type: text/event-stream");
+header("Cache-Control: no-cache");
+header("Connection: keep-alive");
+
+// header("Access-Control-Allow-Headers: Origin, Methods, Content-Type");
+// header("Access-Control-Allow-Methods: GET, OPTIONS");
+// header("Access-Control-Allow-Methods: POST");
+// header('Access-Control-Allow-Origin: *');
+
+//$queryString = $_SERVER['QUERY_STRING'];
+//echo 'start dump';
+//var_dump($queryString);
+//echo 'end dump';
+
+if (isset($_GET['game_id'])){
+    $game_id = $_GET['game_id'];
+    //echo $game_id;
+} else {
+    parse_str($_SERVER['QUERY_STRING'], $get_arr);
+    $game_id = $get_arr['game_id'];
+    //print_r($get_arr);
+}
+if (isset($_GET['last_seen_tick'])) {
+    $last_seen_tick = (int)$_GET["last_seen_tick"];
+    //echo $last_seen_tick;
+} else {
+    parse_str($_SERVER['QUERY_STRING'], $get_arr);
+    $last_seen_tick = (int)$get_arr["last_seen_tick"];
+    //print_r($get_arr);
+}
+
+
+while (true) {
+    // echo 'data: last_seen_tick = ' . $last_seen_tick, "\n\n";
+    // Get the current game tick
+    $game_json = file_get_contents("games/$game_id/game.json");
+    $game_obj = json_decode($game_json, true);
+    $curr_game_tick = (int)$game_obj['tick'];
+    if ($curr_game_tick > $last_seen_tick) {
+        for ($from = $last_seen_tick; $from < $curr_game_tick; $from ++) {
+            $to = $from + 1;
+            $contents = file_get_contents("games/${game_id}/patch_${from}_${to}.txt");
+            $sse_formatted = preg_replace('/\n/', '\ndata: ', trim($contents));
+            echo "event: patch\n";
+            echo 'data: ' . $sse_formatted , "\n\n";
+        }
+
+        while (ob_get_level() > 0) { ob_end_flush(); }
+        flush();
+        if ( connection_aborted() ) break;
+        $last_seen_tick = $curr_game_tick;
+        usleep(50000);
+    }
+    usleep(50000);
+}
+?>
