1cd76457b80e1f18aa05e494fc494227f8b8fd30
beyarkay <boydrkane@gmail.com>
2021-01-09 11:31:02 +0200

    Feat: Added new onboarding instructions

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-export function keygen() {
-    $.ajax({
-        type: "POST",
-        url: "generate_key.php",
-        data: {},
-        success: function(response) {
-            console.log("Success!");
-            console.log(response);
-        }
-    });
-}
 
 export function get_param(param, defaultvalue) {
     if (window.location.href.indexOf(param) > -1) {
